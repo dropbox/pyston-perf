6f1a8b8d64f2383572e9b03644504819f8aff16d
Chris Toshok <toshok@dropbox.com>
2015-04-01 17:04:02 +0000

    move some things around to clean up the django benchmark
    - move the benchmark_suite/django/ dir into benchmark_suite/lib/
    - rename benchmark_suite/testsite/ to benchmark_suite/django_migrate_testsite/

--- benchmarking/benchmark_suite/django_migrate.py
+++ benchmarking/benchmark_suite/django_migrate.py
@@ -2,12 +2,13 @@
 import os
 import sys
 
-# pyston changes:  add benchmark_suite to sys.path
-sys.path += [os.path.join(os.getcwd(), "benchmark_suite/testsite")]
+# pyston changes:  add our testsite dir to sys.path, as well as the lib/ directory so we can locate django
+sys.path.extend([os.path.join(os.getcwd(), "benchmark_suite/django_migrate_testsite"),
+                 os.path.join(os.path.dirname(__file__), "lib")])
 
 # pyston change: clear the sqlite db so we'll re-run the migration
 try:
-    os.remove("benchmark_suite/testsite/db.sqlite3")
+    os.remove("benchmark_suite/django_migrate_testsite/db.sqlite3")
 except:
     pass
 
@@ -15,4 +16,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite.settings")
 
 from django.core.management import execute_from_command_line
 
-execute_from_command_line(["testsite/manage.py", "migrate"])
+execute_from_command_line(["django_migrate_testsite/manage.py", "migrate"])
